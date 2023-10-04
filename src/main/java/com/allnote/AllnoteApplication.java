518aa8f52abe7e1e4ec572e8483d32ca6080494d
KamilKaczmarkiewicz <kaczmarkiewicz98@gmail.com>
2023-10-04 21:14:02 +0200

    first commit

---
+++ src/main/java/com/allnote/AllnoteApplication.java
@@ -0,0 +1,13 @@
+package com.allnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allnote/AllnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
